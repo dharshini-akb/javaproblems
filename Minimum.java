3787a9fed8260d912edadbc0cab6f81ea667649d
Dharshini akb <dharshiniakb.23it@kongu.edu>
2026-06-09 16:02:12 +0530

    minimum element in an array

---
+++ Minimum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Minimum{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+       
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+         int min=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        System.out.println("maximum element in an array is "+min);
+        
+    }
+}
